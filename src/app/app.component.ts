b511d142897cc743c42a49deadac195732600d8c
Prem Ranjan Shaw <premranjanshaw@gmail.com>
2023-11-26 01:09:12 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild, ViewContainerRef, WritableSignal, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LeftComponent } from "./left/left.component";
@@ -14,12 +14,37 @@ import { RightComponent } from "./right/right.component";
 
 })
 export class AppComponent implements OnInit {
+    @ViewChild('newComponent', { read: ViewContainerRef }) newComponent!: ViewContainerRef
+    firstNameData() {
+        this.firstName.set('souvik')
+    }
+
+    lastNameData() {
+        this.data.set(true)
+        this.lastName.set(['souvik', 'biswas'])
+    }
 
     ngOnInit(): void {
         console.log('main');
 
     }
+    data = signal(false)
 
+    firstName: WritableSignal<string> = signal('prem')
+    lastName: WritableSignal<string[]> = signal(['shaw'])
 
+    fullName = computed(() => {
+        if (this.data())
+            return this.firstName() + ' ' + this.lastName()
+        else {
+            return ''
+        }
+    })
+
+    async loadcomponent() {
+        const newcomp = await (await import('./new-component/new-component.component')).NewComponentComponent
+        this.newComponent.clear()
+        this.newComponent.createComponent(newcomp)
+    }
 
 }
