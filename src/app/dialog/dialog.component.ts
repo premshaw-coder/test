451b97c9fe90b724d755817599bf274d81937cd5
Prem Ranjan Shaw <premranjanshaw@gmail.com>
2023-12-17 21:23:10 +0530

    fix

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -10,7 +10,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { DataService } from '../data.service';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import data from '../../assets/data.json'
 
 @Component({
@@ -27,39 +27,41 @@ export class DialogComponent implements OnInit {
   formGroup!: FormGroup<any>;
   allStatus: string[] = ['To Do', 'In Progress', 'Complete'];
   priorities: string[] = ['High', 'Medium', 'Low'];
-  id!:string
+  id!: string
 
-  constructor(private dataService: DataService, private route:ActivatedRoute) {
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) {
 
   }
   ngOnInit(): void {
-    this.id=this.route.snapshot.params['id']
+    this.id = this.route.snapshot.params['id']
     console.log('this.route.snapshot.params', this.route.snapshot.params)
-    this.initialForm(data.find(task=>task.id == +this.id))
+    this.initialForm(data.find(task => task.id == +this.id))
 
   }
 
-  initialForm(data?:any) {
+  initialForm(data?: any) {
     this.formGroup = new FormGroup({
-      status: new FormControl(data?.status || 'To Do'),
-      priority: new FormControl(data?.priority || ''),
-      dueDate: new FormControl(data?.dueDate || ''),
-      taskName: new FormControl(data?.taskName || ''),
-      taskDescription: new FormControl(data?.taskDescription || '')
+      status: new FormControl(data?.status || 'To Do', Validators.required),
+      priority: new FormControl(data?.priority || '', Validators.required),
+      dueDate: new FormControl(data?.dueDate || '', Validators.required),
+      taskName: new FormControl(data?.taskName || '', Validators.required),
+      taskDescription: new FormControl(data?.taskDescription || '', Validators.required)
     });
   }
 
   saveFormData() {
     if (this.formGroup.invalid) {
-      return this.formGroup.markAllAsTouched()
+      alert('all the form fields are reuired')
+      return ""
     }
     let data2 = this.formGroup.value
     data2.id = this.id || +new Date()
     if (this.id) {
-      let index = data.findIndex((task:any)=>task.id == +this.id)
-      data[index]=data2
+      let index = data.findIndex((task: any) => task.id == +this.id)
+      data[index] = data2
     }
     else this.dataService.createtask(data2)
+    this.router.navigate(['dashboard'])
   }
 
 }
